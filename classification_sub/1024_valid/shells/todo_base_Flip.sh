57d9cf0ec202386ed2fa1d70e000008a453d7751
bub3690 <bub3690@naver.com>
2022-12-13 01:22:36 +0900

    AUX : Update shells

---
+++ classification_sub/1024_valid/shells/todo_base_Flip.sh
@@ -0,0 +1,5 @@
+python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model baseline --sublabel label --wandb True --seed 1004 --tag baseline 1004 Flip --augment Flip --num-workers 20 --project-name BMC_vision_classification
+python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model baseline --sublabel label --wandb True --seed 1005 --tag baseline 1005 Flip --augment Flip --num-workers 20 --project-name BMC_vision_classification
+python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model baseline --sublabel label --wandb True --seed 1006 --tag baseline 1006 Flip --augment Flip --num-workers 20 --project-name BMC_vision_classification
+python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model baseline --sublabel label --wandb True --seed 1007 --tag baseline 1007 Flip --augment Flip --num-workers 20 --project-name BMC_vision_classification
+python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model baseline --sublabel label --wandb True --seed 1008 --tag baseline 1008 Flip --augment Flip --num-workers 20 --project-name BMC_vision_classification
