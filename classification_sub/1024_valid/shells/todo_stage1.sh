8a6a584b0150981bd729da1fa7913420206a8ee8
bub3690 <bub3690@naver.com>
2022-12-12 22:02:14 +0900

    AUX: Update shells

---
+++ classification_sub/1024_valid/shells/todo_stage1.sh
@@ -0,0 +1,5 @@
+python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model sub_1stage --sublabel label --name "1stage" --wandb True --seed 1004 --tag 1stage 1004 noAugment --num-workers 20 --project-name BMC_vision_classification
+python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model sub_1stage --sublabel label --name "1stage" --wandb True --seed 1005 --tag 1stage 1005 noAugment --num-workers 20 --project-name BMC_vision_classification
+python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model sub_1stage --sublabel label --name "1stage" --wandb True --seed 1006 --tag 1stage 1006 noAugment --num-workers 20 --project-name BMC_vision_classification
+python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model sub_1stage --sublabel label --name "1stage" --wandb True --seed 1007 --tag 1stage 1007 noAugment --num-workers 20 --project-name BMC_vision_classification
+python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model sub_1stage --sublabel label --name "1stage" --wandb True --seed 1008 --tag 1stage 1008 noAugment --num-workers 20 --project-name BMC_vision_classification
--- classification_sub/1024_valid/todo.sh
+++
@@ -1,5 +0,0 @@
-python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model baseline --sublabel label --wandb True --seed 1004 --tag baseline --num-workers 20 --project-name BMC_vision_classification
-python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model baseline --sublabel label --wandb True --seed 1005 --tag baseline --num-workers 20 --project-name BMC_vision_classification
-python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model baseline --sublabel label --wandb True --seed 1006 --tag baseline --num-workers 20 --project-name BMC_vision_classification
-python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model baseline --sublabel label --wandb True --seed 1007 --tag baseline --num-workers 20 --project-name BMC_vision_classification
-python baseline_train.py --batch-size 32 --epochs 40 --lr 0.0001 --model baseline --sublabel label --wandb True --seed 1008 --tag baseline --num-workers 20 --project-name BMC_vision_classification
